5b5cab06d9893939f57d14932099d18063cc1589
sanriyue <443807234@qq.com>
2019-10-14 21:52:27 +0800

    所有用户信息的查询以及分页

---
+++ src/main/java/com/damo/examsys/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.damo.examsys.controller;
+
+import com.damo.examsys.common.JsonBean;
+import com.damo.examsys.entity.User;
+import com.damo.examsys.service.UserService;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sanriyue
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+    @RequestMapping("/allUser.do")
+    public JsonBean<PageInfo<User>> findAllUser(@RequestParam(defaultValue = "1") int pageNum, @RequestParam(defaultValue = "10") int pageSize){
+
+        return new JsonBean<PageInfo<User>>(0,userService.findAllUser(pageNum,pageSize));
+    }
+
+}
--- src/main/java/com/damo/examsys/service/UserService.java
+++ src/main/java/com/damo/examsys/service/UserService.java
@@ -1,11 +1,13 @@
 package com.damo.examsys.service;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
 
 /**
  * @author sanriyue
  */
-@Service
-public interface UserService {
 
+public interface UserService {
+    PageInfo findAllUser (int pageNum, int pageSize);
 }
