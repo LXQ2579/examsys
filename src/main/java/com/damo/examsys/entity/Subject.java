aeb453fa3b7e0ef528f07e22f7012274faa8838e
leizhonggang <2468549595@qq.com>
2019-10-15 08:26:21 +0800

    subject类手动生成get，set方法，去除注解

--- src/main/java/com/damo/examsys/entity/Subject.java
+++ src/main/java/com/damo/examsys/entity/Subject.java
@@ -10,10 +10,25 @@ import java.io.Serializable;
  * @author liujiulong
  * @date 2019/10/14  14:39:00
  */
-@Data
+
 public class Subject implements Serializable {
 
     private Integer sId;
     private String sName;
 
+    public Integer getsId() {
+        return sId;
+    }
+
+    public void setsId(Integer sId) {
+        this.sId = sId;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
 }
